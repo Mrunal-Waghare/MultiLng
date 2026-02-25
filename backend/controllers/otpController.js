3079ac414b26fd36f9552b6df0943f01a6171495
Mrunal-Waghare <mrunalwaghare@gmail.com>
2026-02-25 18:43:00 +0530

    Fix Gmail SMTP config

--- backend/controllers/otpController.js
+++ backend/controllers/otpController.js
@@ -5,13 +5,17 @@ const bcrypt = require('bcrypt');
 
 // ================= EMAIL SETUP =================
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
-
 // ================= SEND OTP =================
 exports.sendOtp = async (req, res) => {
   try {
